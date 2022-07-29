e769b1f881c0856d46cc0cb90534265d6222a120
🐼 Samrose Ahmed 🐼 <samroseahmed@gmail.com>
2022-07-29 12:04:05 +0300

    website wip dirty stage

--- website/src/components/HomepageFeatures/index.tsx
+++ website/src/components/HomepageFeatures/index.tsx
@@ -72,7 +72,7 @@ function Feature({title, Svg, description}: FeatureItem) {
         <Svg className={styles.featureSvg} role="img" />
       </div>
       <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
+        <h3 style={{ fontFamily: "Lexend" }}>{title}</h3>
         <p>{description}</p>
       </div>
     </div>
