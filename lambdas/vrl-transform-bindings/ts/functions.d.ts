8a14bd8d28777904e780bc947f2a8d76a89a329d
shaeqahmed <shaeq.ahmed@gmail.com>
2022-08-03 07:34:01 +0000

    remove deleted files

--- lambdas/vrl-transform-bindings/ts/functions.d.ts
+++
@@ -1,8 +0,0 @@
-export type Outcome = {
-  success?: {
-    output?: any;
-    result: any;
-  };
-} & { error?: string };
-export function vrl_function_info(): null;
-export function _vrl(program: string, event: any, tz: any): Outcome;
