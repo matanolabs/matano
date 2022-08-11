30d9fec98f81221f3568e147fcd6e52da8414906
Samrose-Ahmed <Samrose-Ahmed@users.noreply.github.com>
2022-08-11 20:05:09 +0000

    deploy: 8f75359ef1d8a574ff41bf11f5463a23747c5166

--- assets/js/814f3328.bdd1d6dc.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_matano_website=self.webpackChunk_matano_website||[]).push([[2535],{45641:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Announcing Matano","permalink":"/blog/2022/08/11/announcing-matano"},{"title":"Placeholder","permalink":"/blog/2022/07/28/placeholder"}]}')}}]);
---
+++ assets/js/b2b675dd.75707ec6.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_matano_website=self.webpackChunk_matano_website||[]).push([[533],{28017:e=>{e.exports=JSON.parse('{"permalink":"/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}')}}]);
