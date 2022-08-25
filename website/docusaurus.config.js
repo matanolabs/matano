98c54c1b73eaf3585fa17fa0837465bc5ba66841
🐼 Samrose Ahmed 🐼 <samroseahmed@gmail.com>
2022-08-25 22:18:06 +0400

    minor fixes

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -180,7 +180,7 @@ const config = {
               },
               {
                 label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
+                href: 'https://twitter.com/matanolabs',
               },
             ],
           },
