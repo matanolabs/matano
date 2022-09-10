46ad98d64f5826d0a770e938f63eba84ea2cf608
Samrose-Ahmed <Samrose-Ahmed@users.noreply.github.com>
2022-09-10 07:50:21 +0000

    deploy: bf30704752ac756ce7ccd1be0dfe65c846aa8bcd

--- assets/js/814f3328.861f71b9.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_matano_website=self.webpackChunk_matano_website||[]).push([[2535],{45641:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"ingesting-data-iceberg","permalink":"/blog/2022/09/06/ingesting-data-iceberg"},{"title":"Announcing Matano","permalink":"/blog/2022/08/11/announcing-matano"}]}')}}]);
