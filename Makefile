12fd1c54300090ae6a53bd8467747f4a74b97d8f
RoseSecurity <michaeljrosenfeld22@gmail.com>
2024-07-19 00:06:31 -0500

    Support Multi-Architecture Builds

--- Makefile
+++ Makefile
@@ -1,4 +1,3 @@
-
 clean: clean-infra clean-cli
 	rm -rf scripts/packaging/node_modules
 
@@ -23,10 +22,29 @@ build-python-rust:
 build-python: build-python-rust
 	rm -rf local-assets/MatanoDetectionsCommonLayer && cd lib/python/matano_detection && $(MAKE) release
 
-build-rust:
+build-rust-linux-x64:
 	cd lib/rust && PYO3_CROSS_PYTHON_VERSION=3.9 cargo lambda build --target x86_64-unknown-linux-gnu.2.26 --release --workspace && \
 	mkdir -p ${CURDIR}/local-assets && \
 	cp -a target/lambda/* ${CURDIR}/local-assets
+
+build-rust-linux-arm:
+	cd lib/rust && PYO3_CROSS_PYTHON_VERSION=3.9 cargo lambda build --target aarch64-unknown-linux-gnu.2.26 --release --workspace && \
+	mkdir -p ${CURDIR}/local-assets && \
+	cp -a target/lambda/* ${CURDIR}/local-assets
+
+build-rust-macos-x64:
+	cd lib/rust && PYO3_CROSS_PYTHON_VERSION=3.9 cargo lambda build --target x86_64-apple-darwin --release --workspace && \
+	mkdir -p ${CURDIR}/local-assets && \
+	cp -a target/lambda/* ${CURDIR}/local-assets
+
+build-rust-macos-arm:
+	cd lib/rust && PYO3_CROSS_PYTHON_VERSION=3.9 cargo lambda build --target aarch64-apple-darwin --release --workspace && \
+	mkdir -p ${CURDIR}/local-assets && \
+	cp -a target/lambda/* ${CURDIR}/local-assets
+
+build-rust-linux: build-rust-linux-x64 build-rust-linux-arm
+build-rust-macos: build-rust-macos-x64 build-rust-macos-arm
+build-rust: build-rust-linux build-rust-macos
 
 build-jvm:
 	cd lib/java/matano && ./gradlew release
--- scripts/packaging/build.js
+++ scripts/packaging/build.js
@@ -22,13 +22,13 @@ process.chdir(workDir);
 function getCommitHash() {
   let commitHash = undefined;
   if (process.env.GITHUB_SHA) {
-    commitHash = process.env.GITHUB_SHA;
+	commitHash = process.env.GITHUB_SHA;
   } else {
-    try {
-      commitHash = execSync("git rev-parse HEAD").toString();
-    } catch (e) {
-      // fail locally, no big deal.
-    }
+	try {
+	  commitHash = execSync("git rev-parse HEAD").toString();
+	} catch (e) {
+	  // fail locally, no big deal.
+	}
   }
   return commitHash?.substring(0, 7);
 }
@@ -47,19 +47,24 @@ function prepareCdkPkg() {
 
   // some js template files mess up pkg, just delete
   fs.rmSync(path.resolve(workDir, "node_modules/aws-cdk/lib/init-templates/app/javascript"), {
-    recursive: true,
-    force: true,
+	recursive: true,
+	force: true,
   });
   fs.rmSync(path.resolve(workDir, "node_modules/aws-cdk/lib/init-templates/sample-app/javascript"), {
-    recursive: true,
-    force: true,
+	recursive: true,
+	force: true,
   });
 
   const cdkPkgConfig = {
-    scripts: ["node_modules/aws-cdk/**/*.js"],
-    assets: ["node_modules/aws-cdk/**"],
-    targets: ["node14.18.1-linux-x64", "node14.18.1-macos-x64"],
-    outputPath: workOutputDir,
+	scripts: ["node_modules/aws-cdk/**/*.js"],
+	assets: ["node_modules/aws-cdk/**"],
+	targets: [
+	  "node14.18.1-linux-x64",
+	  "node14.18.1-macos-x64",
+	  "node14.18.1-linux-arm64",
+	  "node14.18.1-macos-arm64"
+	],
+	outputPath: workOutputDir,
   };
 
   fs.writeFileSync(cdkPkgConfigPath, JSON.stringify(cdkPkgConfig));
@@ -76,19 +81,19 @@ const MShelp = (archiveName) => `\
 Installs the Matano CLI
 
 USAGE:
-    ./${archiveName} [OPTIONS] [--] [ADDITIONAL OPTIONS]
+	./${archiveName} [OPTIONS] [--] [ADDITIONAL OPTIONS]
 
 OPTIONS:
-    --target <path>       The directory to install the Matano CLI. By
-                              default, this directory is: /usr/local/matano-cli
+	--target <path>       The directory to install the Matano CLI. By
+							  default, this directory is: /usr/local/matano-cli
 
-    (See below for a full list of options)
+	(See below for a full list of options)
 
 ADDITIONAL OPTIONS:
 
-    --bin-dir <path>      The directory to store symlinks to executables
-                              for the Matano CLI. By default, the directory
-                              used is: /usr/local/bin
+	--bin-dir <path>      The directory to store symlinks to executables
+							  for the Matano CLI. By default, the directory
+							  used is: /usr/local/bin
 `;
 function getMSHelp(archiveName) {
   const fpath = path.join(baseTempDir, randStr());
@@ -101,23 +106,23 @@ async function main() {
   const localAssetDir = path.join(workDir, "local-assets");
   const localAssetSubDirs = await fs.promises.readdir(localAssetDir);
   localAssetSubDirs.forEach((subdir) => {
-    const subpath = path.join(localAssetDir, subdir);
-    execSync(
-      `zip -r ${subdir}.zip ./* && cp ${subdir}.zip ${localAssetDir} && cd ${localAssetDir} && rm -rf ${subpath}`,
-      {
-        cwd: subpath,
-      }
-    );
+	const subpath = path.join(localAssetDir, subdir);
+	execSync(
+	  `zip -r ${subdir}.zip ./* && cp ${subdir}.zip ${localAssetDir} && cd ${localAssetDir} && rm -rf ${subpath}`,
+	  {
+		cwd: subpath,
+	  }
+	);
   });
 
   await exec([
-    "--no-bytecode",
-    "--public",
-    "--public-packages",
-    "*",
-    "-c",
-    path.join(projDir, "infra.pkg.json"),
-    path.resolve(projDir, "infra/dist/bin/app.js"),
+	"--no-bytecode",
+	"--public",
+	"--public-packages",
+	"*",
+	"-c",
+	path.join(projDir, "infra.pkg.json"),
+	path.resolve(projDir, "infra/dist/bin/app.js"),
   ]);
 
   setCliPackageVersion();
@@ -125,49 +130,47 @@ async function main() {
 
   prepareCdkPkg();
   await exec([
-    "--no-bytecode",
-    "--public",
-    "--public-packages",
-    "*",
-    "-c",
-    cdkPkgConfigPath,
-    "node_modules/aws-cdk/bin/cdk",
+	"--no-bytecode",
+	"--public",
+	"--public-packages",
+	"*",
+	"-c",
+	cdkPkgConfigPath,
+	"node_modules/aws-cdk/bin/cdk",
   ]);
 
   process.chdir(workDir);
   const makeself = installMakeself();
 
-  // TODO: change for arm
   for (const target of ["linux", "macos"]) {
-    const targetSubDir = path.join(workOutputDir, target);
-    execSync(`mkdir -p ${targetSubDir}`);
+	for (const arch of ["x64", "arm64"]) {
+	  const targetSubDir = path.join(workOutputDir, `${target}-${arch}`);
+	  execSync(`mkdir -p ${targetSubDir}`);
 
-    const executables = (await fs.promises.readdir(workOutputDir)).filter((fn) => fn.includes("-" + target));
-    for (const executable of executables) {
-      const simpleExecutableName = executable
-        .replace(`-${target}-x64`, "")
-        .replace(`-${target}-arm64`, "")
-        .replace(`-${target}`, "");
-      execSync(`mv ${path.join(workOutputDir, executable)} ${targetSubDir}/${simpleExecutableName}`);
-    }
-    execSync(`cp -a ${workDir}/local-assets ${targetSubDir}`);
-    execSync(`cp ${path.join(scriptDir, "post-install.sh")} ${targetSubDir}`);
+	  const executables = (await fs.promises.readdir(workOutputDir)).filter((fn) => fn.includes(`-${target}-${arch}`));
+	  for (const executable of executables) {
+		const simpleExecutableName = executable.replace(`-${target}-${arch}`, "");
+		execSync(`mv ${path.join(workOutputDir, executable)} ${targetSubDir}/${simpleExecutableName}`);
+	  }
+	  execSync(`cp -a ${workDir}/local-assets ${targetSubDir}`);
+	  execSync(`cp ${path.join(scriptDir, "post-install.sh")} ${targetSubDir}`);
 
-    const archiveName = `matano-${target}-x64.sh`;
-    execSync(
-      `${makeself} --nochown --help-header ${getMSHelp(
-        archiveName
-      )} --target /usr/local/matano-cli ${targetSubDir} ${archiveName} "Matano CLI" ./post-install.sh`
-    );
-    execSync(`chmod 777 ${archiveName}`);
+	  const archiveName = `matano-${target}-${arch}.sh`;
+	  execSync(
+		`${makeself} --nochown --help-header ${getMSHelp(
+		  archiveName
+		)} --target /usr/local/matano-cli ${targetSubDir} ${archiveName} "Matano CLI" ./post-install.sh`
+	  );
+	  execSync(`chmod 777 ${archiveName}`);
 
-    execSync(`cp ${archiveName} ${projDir}`);
+	  execSync(`cp ${archiveName} ${projDir}`);
 
-    if (process.env.MTN_GPG_SIGN) {
-      console.log("GPG Signing artifact...");
-      execSync(`gpg --output ${archiveName}.sig --detach-sig ${archiveName}`);
-      execSync(`cp ${archiveName}.sig ${projDir}`);
-    }
+	  if (process.env.MTN_GPG_SIGN) {
+		console.log("GPG Signing artifact...");
+		execSync(`gpg --output ${archiveName}.sig --detach-sig ${archiveName}`);
+		execSync(`cp ${archiveName}.sig ${projDir}`);
+	  }
+	}
   }
 
   execSync(`rm -rf ${workDir}`);
